43590f5139aa46f40717a0877290e483f970d189
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-10-05 17:32:14 +0530

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+        <Navbar />
+        </div>
+
+        Body
+
+        <div>
+            <Footer />
+        </div>
+    </div>
+  )
+}
+
+export default Home;
