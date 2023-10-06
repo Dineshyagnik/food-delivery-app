14adb22ea0c6a5b280c81eb66a49a0133ce55e36
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-10-06 17:56:23 +0530

    navbar,footer,card,carousel

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Card from '../components/Card';
+import Carousel from '../components/Carousel';
 
 const Home = () => {
   return (
     <div>
-        <div>
+      <div>
         <Navbar />
-        </div>
+      </div>
 
-        Body
+      <div>
+        <Carousel />
+      </div>
 
-        <div>
-            <Footer />
-        </div>
+      <div className='m-3'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <div>
+        <Footer />
+      </div>
     </div>
   )
 }
