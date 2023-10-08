9693de9dc418e8269aa782226faffda1854288ae
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-10-08 12:31:49 +0530

    database connectivity issue

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://dinesh:dyyayavar7491@gmail.com@cluster0.snfr0jn.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://dinesh:dyyayavar7491@gmail.com@cluster0.snfr0jn.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 
 const mongoDB = async () => {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
-        console.log('connected');
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async(err, res) => {
+        if (err) console.log('---', err);
+        else {
+            console.log('connected');
+
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(function (err, data) {
+                if (err) console.log(err);
+                else console.log(data);
+            })
+        }
+
+
     });
 }
 
