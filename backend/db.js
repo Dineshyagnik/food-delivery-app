b2b2a767b61f2aa6dba584de976d441c29a955c7
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-10-10 18:35:07 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://dinesh:dyyayavar7491@gmail.com@cluster0.snfr0jn.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 
 const mongoDB = async () => {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async(err, res) => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, res) => {
         if (err) console.log('---', err);
         else {
             console.log('connected');
--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const port = 5000
 const mongoDB = require('./db')
 mongoDB();
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Food Delivery App')
 })
 
 app.listen(port, () => {
